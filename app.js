51641668d46a4b9eb7604e70ac53df976dfaa224
rt1893 <37836318+rt1893@users.noreply.github.com>
2025-02-22 14:18:09 +0000

    ch7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('https');
+const http = require('http');
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 4000;
@@ -6,9 +6,9 @@ const port = process.env.PORT || 4000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, World! Its http again\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at https://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
